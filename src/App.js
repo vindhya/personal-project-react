75c15e0da6791e1afc1fab2832c0af415692b0d1
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-26 18:52:13 -0400

    rendering initial username form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  grommet,
+  Box,
+  Button,
+  Form,
+  FormField,
+  Grommet,
+  TextInput
+} from 'grommet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// mockup button colour: '#57BE9D'
+
+class App extends Component {
+  state = { value: '' };
+
+  handleChange = event => {
+    const {
+      target: { value }
+    } = event;
+    this.setState({ value });
+  };
+
+  handleSubmit = event => {
+    console.log('state on submit', this.state);
+    event.preventDefault();
+  };
+
+  render() {
+    return (
+      <Grommet full theme={grommet}>
+        <Box fill align="center" justify="center">
+          <Box width="medium">
+            <Form onSubmit={this.handleSubmit}>
+              <FormField
+                label="GitHub Username"
+                htmlFor="username-input"
+                {...this.props}
+              >
+                <TextInput
+                  id="username-input"
+                  placeholder="Type something..."
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                  required
+                  // TODO: figure out validation for no spaces
+                  // validate={{ regexp: /^\S*$/ }}
+                />
+              </FormField>
+              <Button
+                primary
+                color="accent-1"
+                type="submit"
+                label="Get User"
+              ></Button>
+            </Form>
+          </Box>
+        </Box>
+      </Grommet>
+    );
+  }
 }
 
 export default App;
