f6cad16ad139f8bb32e8998674f33c34f9d7453e
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-27 19:04:47 -0400

    main text for each list item is an anchor tag linking to it on github

--- src/components/EventList/EventListItem.js
+++ src/components/EventList/EventListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text } from 'grommet';
+import { Anchor, Text } from 'grommet';
 
 import ListItem from '../List/ListItem';
 import { capitalize } from '../../utils/utils';
@@ -7,17 +7,22 @@ import { capitalize } from '../../utils/utils';
 const EventListItem = ({ event, index, type }) => {
   let mainText = '';
   let subText = '';
+  let url = '';
   if (event && type === 'fork') {
     mainText = event.payload.forkee.full_name;
     subText = `Forked from: ${event.repo.name}`;
+    url = event.payload.forkee.html_url;
   } else if (event && type === 'pr') {
     mainText = event.payload.pull_request.title;
     subText = `Status: ${capitalize(event.payload.pull_request.state)}`;
+    url = event.payload.pull_request.html_url;
   }
 
   return (
     <ListItem index={index}>
-      <Text>{mainText}</Text>
+      <Anchor href={url} target="_blank">
+        {mainText}
+      </Anchor>
       <Text size="small">{subText}</Text>
     </ListItem>
   );
