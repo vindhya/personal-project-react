5cdae211e782829524d371ee9930a0346fa11f30
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-29 04:25:36 -0400

    added describe

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,15 +4,17 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App component tests', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('renders the username input', () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/github username/i)).toBeInTheDocument();
-});
+  it('renders the username input', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/github username/i)).toBeInTheDocument();
+  });
 
-xit('renders the user display', () => {});
+  xit('renders the user display', () => {}); 
+});
