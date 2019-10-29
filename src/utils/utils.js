5c1ff42b75dc6f2f2d3b897e3f95ae0e9d0ae9d9
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-29 03:15:52 -0400

    copied tests from other branch and making utils less bruttle

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,16 @@
 import { API_PREFIX, API_SUFFIX } from '../constants';
 
+// what is the correct return value when invalid parameters are passed?
+// i.e., parameter(s) not defined
+
 export const capitalize = str => {
-  const strArray = str.split('');
-  strArray[0] = strArray[0].toUpperCase();
-  return strArray.join('');
+  if (str) {
+    const strArray = str.split('');
+    strArray[0] = strArray[0].toUpperCase();
+    return strArray.join('');
+  }
+  return null;
 };
 
-export const getEventApiUrl = username => API_PREFIX + username + API_SUFFIX;
+export const getEventApiUrl = username =>
+  username ? API_PREFIX + username + API_SUFFIX : null;
