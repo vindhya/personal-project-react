8d573f126c830bd6aab9e0eb7bca02189f669750
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-10-30 18:32:27 -0400

    removed unnecessary curly brackets

--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -4,15 +4,12 @@ const INITIAL_STATE = { showUser: false, username: '', inputValue: '' };
 
 export const userReducer = (state = INITIAL_STATE, action = {}) => {
   switch (action.type) {
-    case USER_ACTION_TYPE.TOGGLE_SHOW_USER: {
+    case USER_ACTION_TYPE.TOGGLE_SHOW_USER:
       return { ...state, showUser: !state.showUser };
-    }
-    case USER_ACTION_TYPE.SET_USERNAME: {
+    case USER_ACTION_TYPE.SET_USERNAME:
       return { ...state, username: action.payload };
-    }
-    case USER_ACTION_TYPE.SET_INPUT_VALUE: {
+    case USER_ACTION_TYPE.SET_INPUT_VALUE:
       return { ...state, inputValue: action.payload };
-    }
     default:
       return state;
   }
